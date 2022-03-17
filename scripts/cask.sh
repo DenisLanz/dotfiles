a3ec8ac7ffe6403a81c99b1c0b28d6b6550d5328
DenisLanz <denis.lanz@gmail.com>
2022-03-17 19:54:56 +0100

    switch to array for casks

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 echo ""
 echo "#############################"
@@ -11,7 +11,6 @@ ROOT_DIR=$1
 
 echo "installing VSCode"
 brew uninstall --cask --force visual-studio-code && brew install --cask visual-studio-code
-# brew uninstall --cask --force visual-studio-code-insiders && brew install --cask visual-studio-code-insiders
 
 if [ -f ~/Library/Application\ Support/Code/User/settings.json ]; then
   echo "found old visual studio code settings. removing..."
@@ -22,48 +21,38 @@ ln -s "$ROOT_DIR/config/visual-studio-code-settings.json" ~/Library/Application\
 
 brew tap homebrew/cask-versions
 
-# echo "installing custom fonts"
-# brew tap homebrew/cask-fonts
-# brew install --cask font-fira-code
-# Fira code with fancy italics
-#open "$ROOT_DIR/static/fira-code-iscript/FiraCodeiScript-Regular.ttf"
-#open "$ROOT_DIR/static/fira-code-iscript/FiraCodeiScript-Bold.ttf"
-#open "$ROOT_DIR/static/fira-code-iscript/FiraCodeiScript-Italic.ttf"
 
-#brew install --cask font-oswald
-#brew install --cask font-ubuntu
-# terminal font
-#brew install --cask font-caskaydia-cove-nerd-font
-#brew install --cask graphql-playground
+CASKS=(
+google-chrome
+chromium
+firefox
+iterm2
+qlmarkdown
+spotify
+bartender
+rocket
+monitorcontrol
+alfred
+contexts
+maestral
+marked2
+divvy
+keycastr
+transmit
+little-snitch
+fliqlo
+monitorcontrol
+typeface
+fig
+micro-snitch
+hazel
+airfoil
+)
 
-# mardown quicklook
-# https://github.com/toland/qlmarkdown/
-brew install --cask qlmarkdown
+for cask in "${CASKS[@]}"
+do
+	brew install --cask "$cask"
+done
 
-# Browser
-brew install --cask google-chrome
-brew install --cask chromium
-brew install --cask firefox
-
-# iterm terminal replacement
-brew install --cask iterm2
 # install iterm utilities right away
 curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
-
-brew install --cask spotify
-brew install --cask bartender
-brew install --cask rocket
-brew install --cask monitorcontrol
-brew install --cask alfred
-brew install --cask contexts
-brew install --cask maestral
-brew install --cask marked2
-brew install --cask divvy
-brew install --cask keycastr
-brew install --cask transmit
-brew install --cask little-snitch
-brew install --cask fliqlo
-brew install --cask monitorcontrol
-brew install --cask typeface
-brew install --cask fig
-brew install --cask micro-snitch
