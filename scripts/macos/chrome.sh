255f07c228e8d9d01f57ee50efb0fa12d2788eb9
Denis Lanz <5640155+DenisLanz@users.noreply.github.com>
2023-11-22 13:17:21 +0100

    rm outdated dotfiles
    Signed-off-by: Denis Lanz <5640155+DenisLanz@users.noreply.github.com>

--- scripts/macos/chrome.sh
+++
@@ -1,7 +0,0 @@
-echo "Use the system-native print preview dialog in chrome"
-defaults write com.google.Chrome DisablePrintPreview -bool true
-defaults write com.google.Chrome.canary DisablePrintPreview -bool true
-
-echo "Expand the print dialog by default in Chrome"
-defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
-defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
--- scripts/macos/textedit.sh
+++
@@ -1,6 +0,0 @@
-echo "Use plain text mode for new TextEdit documents"
-defaults write com.apple.TextEdit RichText -int 0
-
-echo "Open and save files as UTF-8 in TextEdit"
-defaults write com.apple.TextEdit PlainTextEncoding -int 4
-defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
